6293035dfa2aff672d8b9db17e74b1c512550c2d
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-01-14 19:23:33 +0900

    Setup riverpod

---
+++ lib/main/main_ui_model_provider.dart
@@ -0,0 +1,16 @@
+import 'package:ikut_annotation_v2/main/main_ui_model.dart';
+import 'package:ikut_annotation_v2/model/label_image.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main_ui_model_provider.g.dart';
+
+@riverpod
+MainUiModel mainUiModel(MainUiModelRef ref) {
+  return const MainUiModel(
+      images: [
+        LabeledImage(path: "image_01.png", label: "sushi"),
+      ],
+      imageIndex: 0,
+      previousImageIndex: 0,
+      labels: ["takoyaki", "sushi", "gyoza"]);
+}
