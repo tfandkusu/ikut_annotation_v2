ac5ea91825d1aa461ae70cf766fde8e176cdf14f
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-01-15 01:12:25 +0900

    Fix analyze error

--- test/provider_container.dart
+++ test/provider_container.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:riverpod/riverpod.dart';
 
 /// https://riverpod.dev/ja/docs/essentials/testing
 ///
