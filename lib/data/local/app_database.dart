9256c0418be93ec8203a6eba665e9d741814348c
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-03-07 01:08:02 +0900

    Add Drift's table definitions.

---
+++ lib/data/local/app_database.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:drift/native.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
+part 'app_database.g.dart';
+
+class LocalLabels extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  TextColumn get name => text()();
+}
+
+class LocalImages extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  TextColumn get url => text()();
+
+  IntColumn get labelId =>
+      integer().customConstraint('NOT NULL REFERENCES local_labels(id)')();
+}
+
+@DriftDatabase(tables: [LocalImages, LocalLabels])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(join(dbFolder.path, 'app.sqlite3'));
+    if (Platform.isAndroid) {
+      await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+    }
+    final cacheBase = (await getTemporaryDirectory()).path;
+    sqlite3.tempDirectory = cacheBase;
+    return NativeDatabase.createInBackground(file);
+  });
+}
