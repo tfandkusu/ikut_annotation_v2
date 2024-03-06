c0a5886509db70c2b4e832a04c242e2eb76f24eb
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-03-07 03:25:24 +0900

    rename file name

--- lib/model/labeled_image.dart
+++ lib/model/labeled_image.dart
@@ -1,5 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-part 'label_image.freezed.dart';
+part 'labeled_image.freezed.dart';
 
 @freezed
 class LabeledImage with _$LabeledImage {
