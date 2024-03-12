26de5f8b67c97caba1d14c0c20e2c8901bcb97da
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-03-13 05:01:50 +0900

    Make SelectionEventHandler

--- lib/screen/selection/stateholder/selection_event_handler.dart
+++ lib/screen/selection/stateholder/selection_event_handler.dart
@@ -2,6 +2,9 @@ import 'package:ikut_annotation_v2/data/image_index_state_notifier_provider.dart
 import 'package:ikut_annotation_v2/data/repository.dart';
 import 'package:ikut_annotation_v2/screen/selection/stateholder/selection_ui_model.dart';
 import 'package:ikut_annotation_v2/screen/selection/stateholder/selection_ui_model_state_notifier_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'selection_event_handler.g.dart';
 
 class SelectionEventHandler {
   final SelectionUiModelStateNotifier _stateNotifier;
@@ -19,10 +22,27 @@ class SelectionEventHandler {
     _stateNotifier.setAnnotationTaskUrl(annotationTaskUrl);
   }
 
-  Future<void> load(String taskUrl) async {
+  Future<void> onClickLoad(String taskUrl) async {
     _stateNotifier.setProgress(true);
     await _repository.load(taskUrl);
     _imageIndexStateNotifier.reset();
     _stateNotifier.setBackEffect(true);
   }
+
+  void onClickAnnotationTaskGuide() {
+    _stateNotifier.setShowAnnotationTaskGuideEffect(true);
+  }
+
+  void onShownAnnotationTaskGuide() {
+    _stateNotifier.setShowAnnotationTaskGuideEffect(false);
+  }
+}
+
+@riverpod
+SelectionEventHandler selectionEventHandler(SelectionEventHandlerRef ref) {
+  return SelectionEventHandler(
+    ref.read(selectionUiModelStateNotifierProvider.notifier),
+    ref.read(repositoryProvider),
+    ref.read(imageIndexStateNotifierProvider.notifier),
+  );
 }
---
+++ test/screen/selection/stateholder/selection_event_handler_test.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ikut_annotation_v2/data/image_index_state_notifier_provider.dart';
+import 'package:ikut_annotation_v2/data/repository.dart';
+import 'package:ikut_annotation_v2/model/image_index.dart';
+import 'package:ikut_annotation_v2/screen/selection/stateholder/selection_event_handler.dart';
+
+import 'package:ikut_annotation_v2/screen/selection/stateholder/selection_ui_model.dart';
+import 'package:ikut_annotation_v2/screen/selection/stateholder/selection_ui_model_state_notifier_provider.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../util/helper.dart';
+import '../../../util/provider_container.dart';
+
+class SelectionUiModelStateNotifierMock
+    extends AutoDisposeNotifier<SelectionUiModel>
+    with Mock
+    implements SelectionUiModelStateNotifier {}
+
+class ImageIndexStateNotifierMock extends Notifier<ImageIndex>
+    with Mock
+    implements ImageIndexStateNotifier {}
+
+class RepositoryMock extends Mock implements Repository {}
+
+void main() {
+  late ProviderContainer container;
+  late SelectionEventHandler eventHandler;
+  late RepositoryMock repository;
+  late SelectionUiModelStateNotifierMock stateNotifier;
+  late ImageIndexStateNotifierMock imageIndexStateNotifier;
+  setUp(() {
+    repository = RepositoryMock();
+    stateNotifier = SelectionUiModelStateNotifierMock();
+    imageIndexStateNotifier = ImageIndexStateNotifierMock();
+    container = createContainer(
+      overrides: [
+        repositoryProvider.overrideWith((ref) => repository),
+        selectionUiModelStateNotifierProvider.overrideWith(() => stateNotifier),
+        imageIndexStateNotifierProvider
+            .overrideWith(() => imageIndexStateNotifier)
+      ],
+    );
+    eventHandler = container.read(selectionEventHandlerProvider);
+  });
+  tw("selectAnnotationTaskKind", () {
+    tt("uiModel's selectedAnnotationTaskKind is updated", () {
+      eventHandler.selectAnnotationTaskKind(AnnotationTaskKind.yours);
+      verify(() => stateNotifier
+          .setSelectedAnnotationTaskKind(AnnotationTaskKind.yours));
+    });
+  });
+  tw("setAnnotationTaskUrl", () {
+    tt("uiModel's annotationTaskUrl is updated", () {
+      const annotationTaskUrl = "https://example.com/task.yaml";
+      eventHandler.setAnnotationTaskUrl(annotationTaskUrl);
+      verify(() => stateNotifier.setAnnotationTaskUrl(annotationTaskUrl));
+    });
+  });
+  tw("onClickLoad", () {
+    tt("AnnotationTask is loaded, image's index is reset and back to main",
+        () async {
+      const taskUrl = "https://example.com/task.yaml";
+      when(() => repository.load(taskUrl)).thenAnswer((_) async {});
+      await eventHandler.onClickLoad(taskUrl);
+      verifyInOrder([
+        () => stateNotifier.setProgress(true),
+        () => repository.load(taskUrl),
+        () => imageIndexStateNotifier.reset(),
+        () => stateNotifier.setBackEffect(true),
+      ]);
+    });
+  });
+  tw("onClickAnnotationTaskGuide", () {
+    tt("uiModel's howAnnotationTaskGuideEffect is true", () {
+      eventHandler.onClickAnnotationTaskGuide();
+      verify(() => stateNotifier.setShowAnnotationTaskGuideEffect(true));
+    });
+  });
+  tw("onShownAnnotationTaskGuide", () {
+    tt("uiModel's howAnnotationTaskGuideEffect is false", () {
+      eventHandler.onShownAnnotationTaskGuide();
+      verify(() => stateNotifier.setShowAnnotationTaskGuideEffect(false));
+    });
+  });
+}
