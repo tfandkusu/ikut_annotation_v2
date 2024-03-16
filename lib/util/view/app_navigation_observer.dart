7eecec1f0c9ade646ada29969d5ad2a933e65f76
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-03-16 18:56:14 +0900

    Revert "Add AppNavigationObserver"
    This reverts commit 24b003cb22f2ea608c6c9134602f082c815d2319.

--- lib/util/view/app_navigation_observer.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class AppNavigationObserver extends NavigatorObserver {
-  @override
-  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    // final routeName = route.settings.name;
-  }
-}
