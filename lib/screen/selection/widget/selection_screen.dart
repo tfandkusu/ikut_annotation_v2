e09f94ec331114654e41cb03fb6c24fd1db174f9
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-03-16 21:02:34 +0900

    This app can load sample annotation task.

--- lib/screen/selection/widget/selection_screen.dart
+++ lib/screen/selection/widget/selection_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ikut_annotation_v2/util/view/check_one_shot_operation.dart';
 
 import '../../i10n/localization.dart';
 import '../stateholder/selection_event_handler.dart';
@@ -18,23 +19,39 @@ class SelectionScreen extends HookConsumerWidget {
       eventHandler.onCreate();
       return () {};
     }, const []);
+    ref.listen(selectionUiModelProvider, (previous, next) {
+      checkOneShotOperation(previous, next, (uiModel) => uiModel.backEffect,
+          (backEffect) {
+        if (backEffect) {
+          Navigator.pop(context);
+        }
+      });
+    });
     return PopScope(
       canPop: uiModel.canPop,
-      child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: Text(localization.selectionTitle),
-          ),
-          body: Stack(children: [
-            ListView(
+      child: Stack(children: [
+        Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: Text(localization.selectionTitle),
+            ),
+            body: ListView(
               padding: const EdgeInsets.all(16.0),
               children: [
                 _sampleSubtitle(context, localization),
                 _sampleButton(context, eventHandler, localization),
               ],
-            ),
-            if (uiModel.progress) const Center(child: CircularProgressIndicator()),
-          ])),
+            )),
+        if (uiModel.progress)
+          ModalBarrier(
+            color: Colors.black.withOpacity(0.5),
+            dismissible: false,
+          ),
+        if (uiModel.progress)
+          const Center(
+            child: CircularProgressIndicator(),
+          ),
+      ]),
     );
   }
 
